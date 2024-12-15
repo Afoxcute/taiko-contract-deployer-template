c4956f69589fc0f134b354677d7df5fe2941ad7a
Afoxcute <jwavolabilove16@gmail.com>
2024-12-15 12:45:58 +0000

    fixes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,9 +4,10 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
 solidity: "0.8.26",
 networks: {
-    taiko: {
-        url: "https://rpc.hekla.taiko.xyz",
+    mantle: {
+        url: "https://rpc.sepolia.mantle.xyz/",
         accounts: [process.env.YOUR_PRIVATE_KEY],
+        chainId: 5000
     }
 },
 gasPrice: 10000000000,
